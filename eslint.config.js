025311f6619f9cb0b84843ae3f3f9388b9da5bc8
pragmatical1 <salterjon@gmail.com>
2026-06-08 01:27:18 -0500

    Lint frontend and Netlify function files separately

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -23,6 +23,14 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['netlify/functions/**/*.mts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.node,
     },
   }
 );
